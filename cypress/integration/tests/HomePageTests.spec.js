7d5c73e673f3933cdf8167b1783192894d5d2408
jerzokb <beata@palkabm.net>
2021-08-02 21:55:33 +0200

    Cypress

---
+++ cypress/integration/tests/HomePageTests.spec.js
@@ -0,0 +1,26 @@
+beforeEach(() => {
+    cy.visit('https://autodemo.testoneo.com/en/')
+})
+
+it('should test application title', () => {
+    cy.title().should('eq', 'Lost Hat')
+})
+
+it('should test CLOTHES menu option', () => {
+    cy.get('#category-3').click()
+    cy.url().should('eq', 'https://autodemo.testoneo.com/en/3-clothes')
+})
+
+it('should test ACCESSORIES menu option', () => {
+    cy.get('#category-6').click()
+    cy.url().should('eq', 'https://autodemo.testoneo.com/en/6-accessories')
+})
+
+it('should test ACCESSORIES menu option', () => {
+    cy.get('#category-9').click()
+    cy.url().should('eq', 'https://autodemo.testoneo.com/en/9-art')
+})
+
+it('should test slider size', () => {
+    cy.get('.carousel-item').should('have.length', 3)
+})
